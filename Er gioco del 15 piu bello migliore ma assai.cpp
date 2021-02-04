87f6dd5db611ee80b51e47150964d02fda64ce76
AlmondFlame1287 <75363823+AlmondFlame1287@users.noreply.github.com>
2021-02-04 12:25:14 +0100

    Fine sviluppo della difficoltà facile

--- Er gioco del 15 piu bello migliore ma assai.cpp
+++ Er gioco del 15 piu bello migliore ma assai.cpp
@@ -3,34 +3,48 @@
 #include <time.h>
 
 using namespace std;
-void facile()
+void facile() //inizio della difficoltà facile
 {
 	//possibilità di vincita del giocatore praticamente pari a 100
 	int monete = 11;
 	int moneteRaccolteP, moneteRaccolteM;
-	
+	srand(time(NULL));
 
-	cout << "Quante monete vuoi raccogliere? (da 1 a 3)";
-	cin >> moneteRaccolteP;
-	
-	while(moneteRaccolteP <= 0 || moneteRaccolteP >= 4)
+	while(monete > 0)
 	{
-		cout << "Ti ho detto di scegliere tra 1 e 3!";
-		facile();
+
+		cout << "Quante monete vuoi raccogliere? (da 1 a 3)";
+		cin >> moneteRaccolteP;
+	
+		while(moneteRaccolteP <= 0 || moneteRaccolteP >= 4)
+		{
+			cout << "Ti ho detto di scegliere tra 1 e 3!";
+			facile();
+		}
+	
+		monete -= moneteRaccolteP;
+		moneteRaccolteM = rand()%3 + 1;
+		monete -= moneteRaccolteM;
+		cout << "Il computer ha preso " << moneteRaccolteM << " monete. Rimangono " << monete << " monete" << endl;
+	
+		if(monete < 0)
+		{
+			cout << "Il computer ha preso l'ultima moneta, e dunque hai vinto!" << endl;
+		}
+		else if(monete == 0)
+		{
+			cout << "Hai preso l'ultima moneta. Hai perso!" << endl;
+		}
 	}
+}	
+
+/*
+void medio() //inizio della difficoltà media
+{
 	
-	monete -= moneteRaccolteP;
-	moneteRaccolteM = rand()%3;
-	
-	while(moneteRaccolteM == 0)
-	{
-		moneteRaccolteM = rand()%3;
-	}
-	
-	cout << moneteRaccolteM << endl;
 }
 
-
+*/
 
 
 
@@ -48,7 +62,7 @@ int main()
 			facile();
 			break;
 		case 'm':
-//			medio();
+			medio();
 			break;
 		case 'd':
 //			difficile();

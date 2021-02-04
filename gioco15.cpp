1e3e0f2267a7f868b5dfd0c8e89ea584f13ea484
AlmondFlame1287 <75363823+AlmondFlame1287@users.noreply.github.com>
2021-02-04 15:42:27 +0100

    Inizio scrittura difficoltà difficile
    Sono ancora indeciso sul mettere delle varibili di vittoria casuale su tutte le difficoltà oppure  tenerlo in questo modo.

--- gioco15.cpp
+++ gioco15.cpp
@@ -41,6 +41,7 @@ void facile() //inizio della difficoltà facile
 
 void medio() //inizio della difficoltà media
 {
+	//probabilità di vittoria casuale
 	int monete = 11;
 	int moneteRaccolteP, moneteRaccolteM;
 	srand(time(NULL));
@@ -49,7 +50,7 @@ void medio() //inizio della difficoltà media
 	
 	probSconf = rand()%100 + 1;
 	
-	if(probSconf >= 70)
+	if(probSconf >= 60)
 	{
 		while(monete > 0)
 		{
@@ -97,7 +98,70 @@ void medio() //inizio della difficoltà media
 		}	
 	}
 
+	else
+	{
+		while(monete > 0)
+		{
+	
+			cout << "Quante monete vuoi raccogliere? (da 1 a 3)";
+			cin >> moneteRaccolteP;
+		
+			while(moneteRaccolteP <= 0 || moneteRaccolteP >= 4)
+			{
+				cout << "Ti ho detto di scegliere tra 1 e 3!";
+				facile();
+			}
+		
+			monete -= moneteRaccolteP;
+			moneteRaccolteM = rand()%3 + 1;
+			monete -= moneteRaccolteM;
+			cout << "Il computer ha preso " << moneteRaccolteM << " monete. Rimangono " << monete << " monete" << endl;
+		
+			if(monete < 0)
+			{
+				cout << "Il computer ha preso l'ultima moneta, e dunque hai vinto!" << endl;
+			}
+			else if(monete == 0)
+			{
+				cout << "Hai preso l'ultima moneta. Hai perso!" << endl;
+			}
+		}
+	}
 }
+
+
+void difficile() //inizio della difficoltà difficile
+{
+	//possibilità di vincita del giocatore praticamente pari a 0
+	int monete = 9;
+	int moneteRaccolteP, moneteRaccolteM;
+	
+	cout << "Inizia l'AI che prende 2 monete" << endl;
+	cout << "Quante monete vuoi raccogliere? (da 1 a 3)";
+	cin >> moneteRaccolteP;
+	
+	while(moneteRaccolteP <= 0 || moneteRaccolteP >= 4)
+	{
+		cout << "Ti ho detto di scegliere tra 1 e 3!";
+		difficile();
+	} 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+}
+
 
 int main()
 {
@@ -116,7 +180,7 @@ int main()
 			medio();
 			break;
 		case 'd':
-//			difficile();
+			difficile();
 			break;
 		default:
 			cout << "Inserisci una difficoltà valida!";
